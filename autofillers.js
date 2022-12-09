5c4208f34cf2bdb35c0170ff58e29dcd149963fe
Infinityscraft <luisaraniva2@gmail.com>
2022-12-08 18:08:54 -0600

    Create autofillers.js

---
+++ autofillers.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name autofillers script
+// @namespace Low
+// @version 1.4
+// @updateURL https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/obfuscated.users.js
+// @downloadURL https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/obfuscated.users.js
+// @description autofiller
+// @require https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.15.5/xlsx.full.min.js
+// @author ASV
+// @match https://simmow.salud.gob.sv/conrec.php*
+// @match localhost
+// @grant unsafeWindow
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
