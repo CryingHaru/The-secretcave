f44d7b4afbdf0bf35a5e655274440bdca896bbc6
Infinityscraft <luisaraniva2@gmail.com>
2023-01-04 12:15:37 -0600

    Update obfuscated.users.js

--- obfuscated.users.js
+++ obfuscated.users.js
@@ -4,8 +4,9 @@
 // @version  2.5
 // @updateURL https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/obfuscated.users.js
 // @downloadURL https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/obfuscated.users.js
-// @require https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/loaded.users.js
+
 // @description autofiller
+// @require https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/loaded.users.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.15.5/xlsx.full.min.js
 // @resource   IMPORTED_CSS https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/styles.css
