881dc0d05996ff6bfac44b87ff317dd1ca57dc90
Infinityscraft <luisaraniva2@gmail.com>
2023-01-04 12:12:16 -0600

    Update and rename loaded.js to loaded.users.js

--- loaded.users.js
+++ loaded.users.js
@@ -2,8 +2,8 @@
 // @name     autofillers script
 // @namespace   Low
 // @version  1.0
-// @updateURL https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/obfuscated.users.js
-// @downloadURL https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/obfuscated.users.js
+// @updateURL https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/loaded.users.js
+// @downloadURL https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/loaded.users.js
 // @description autofiller
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.15.5/xlsx.full.min.js
