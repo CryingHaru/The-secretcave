9a7ba404e53acf185c1dd2645f8d109c8e1c372f
Infinityscraft <luisaraniva2@gmail.com>
2023-01-04 12:14:45 -0600

    Update loaded.users.js

--- loaded.users.js
+++ loaded.users.js
@@ -1,25 +1,3 @@
-// ==UserScript==
-// @name     autofillers script
-// @namespace   Low
-// @version  1.0
-// @updateURL https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/loaded.users.js
-// @downloadURL https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/loaded.users.js
-// @description autofiller
-// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
-// @require     https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.15.5/xlsx.full.min.js
-// @resource   IMPORTED_CSS https://raw.githubusercontent.com/CryingHaru/The-secretcave/main/styles.css
-// @grant      GM_getResourceText
-// @grant      GM_addStyle
-// @author ASV
-// @match https://simmow.salud.gob.sv/conrec.php*
-// @match localhost
-// @grant unsafeWindow
-// @grant        GM_setValue
-// @grant        GM_getValue
-// ==/UserScript==
-//Variables
-const my_css = GM_getResourceText("IMPORTED_CSS");
-GM_addStyle(my_css);
 let expediente = document.querySelector('[name="expe"]');
 const departamento = document.querySelector('select[name="p_dept"]');
 const municipio = document.querySelector('select[name="p_muni"]');
